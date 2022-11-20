83d48ffd96c258628a1005982b01afeeefe605d0
Edgar Gavrik <edgar.gavrik@gmail.com>
2022-11-20 20:46:22 +0100

    get all files in current dir

--- modstat.sh
+++ modstat.sh
@@ -3,7 +3,7 @@
 Red='\033[0;31m'
 End='\033[0m'
 
-for dir in ../*; do
+for dir in *; do
     if [[ $(find $dir -type d -name ".git") ]]; then
         stat=$(git -C $dir status -s)
         if [[ $stat == *"M"* ]]; then
