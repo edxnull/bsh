7b21fde6296cdb542c101b4f9879a52fcfe04039
Edgar Gavrik <edgar.gavrik@gmail.com>
2024-09-10 21:29:06 +0200

    remove extension and use basename on file

--- gitshou.sh
+++ gitshou.sh
@@ -10,10 +10,12 @@ commit_hash=$1
 files=""
 
 for file in $(git ls-tree -r --name-only $commit_hash); do
-  extension="${file##*.}"
-  temp_file=$(mktemp ./$(uuidgen | cut -c1-8).$file.$extension)
-  git show "$commit_hash:$file" > "$temp_file"
-  files="$files $temp_file"
+    if [[ "$file" == "bin/"* ]]; then
+        continue
+    fi
+    temp_file=$(mktemp ./$(uuidgen | cut -c1-8).$(basename $file))
+    git show "$commit_hash:$file" > "$temp_file"
+    files="$files $temp_file"
 done
 
 echo "Created files: $files"
@@ -21,7 +23,7 @@ echo "Created files: $files"
 vim -p $files
 
 for temp_file in $files; do
-  rm -f "$temp_file"
+    rm -f "$temp_file"
 done
 
 echo "Temporary files have been removed."
